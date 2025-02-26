ed0f7d391cb830f5a828f295cd5f3ca5e9118d11
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2025-02-27 00:30:42 +0500

    Revert back the navbar code

--- src/app/components/sections/header/dropdown.tsx
+++ src/app/components/sections/header/dropdown.tsx
@@ -14,48 +14,31 @@ export default function Dropdown({
   const [showDropdown, setShowDropdown] = useState(false);
 
   return (
-    <li className="relative">
-      <button
-        className="flex gap-1 uppercase w-full lg:w-auto"
-        onClick={() => setShowDropdown(!showDropdown)}
-      >
-        {name} <ChevronDown className={`size-5 text-primary transition-transform ${showDropdown ? "rotate-180" : ""}`} />
+    <li
+      className="relative"
+      onMouseEnter={() => setShowDropdown(true)}
+      onMouseLeave={() => setShowDropdown(false)}
+    >
+      <button className="flex gap-1 uppercase">
+        {name} <ChevronDown className="size-5 text-primary" />
       </button>
-
-      {/* Desktop Mode: Hover Dropdown */}
-      <div className="hidden lg:block">
-        {showDropdown && (
-          <div className="absolute flex flex-col bg-white border justify-start text-start text-nowrap rounded-lg z-[9999] top-5">
-            {links.map((link) => (
-              <Link
-                key={link.name}
-                href={link.link}
-                className="border-b py-3 px-4 hover:text-primary transition-all duration-300 font-semibold"
-                onClick={() => {
-                  setIsMenu(false);
-                  setShowDropdown(false);
-                }}
-              >
-                {link.name}
-              </Link>
-            ))}
-          </div>
-        )}
-      </div>
-
-      {/* Mobile Mode: Inline Expansion */}
-      <div className={`lg:hidden flex flex-col gap-2 mt-2 transition-all duration-300 ${showDropdown ? "block" : "hidden"}`}>
-        {links.map((link) => (
-          <Link
-            key={link.name}
-            href={link.link}
-            className="py-2 px-3 bg-gray-100 rounded-md hover:bg-gray-200 transition-all duration-300 font-medium"
-            onClick={() => setIsMenu(false)}
-          >
-            {link.name}
-          </Link>
-        ))}
-      </div>
+      {showDropdown && (
+        <div className="absolute flex flex-col bg-white border justify-start text-start text-nowrap rounded-lg z-[9999] top-5">
+          {links.map((link) => (
+            <Link
+              key={link.name}
+              href={link.link}
+              className="border-b py-3 px-4 hover:text-primary transition-all duration-300 font-semibold"
+              onClick={() => {
+                setIsMenu(false);
+                setShowDropdown(false); // Close the dropdown
+              }}
+            >
+              {link.name}
+            </Link>
+          ))}
+        </div>
+      )}
     </li>
   );
 }
